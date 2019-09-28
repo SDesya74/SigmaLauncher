3e438cbee9bef3f4c5237b48378661614c413a75
Саня Череп <SDesya74@gmail.com>
2019-09-28 21:58:51 +0300

    Initial commit

--- app/src/main/java/com/dilonexus/sigmalauncher/MainActivity.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/MainActivity.java
@@ -1,14 +1,71 @@
 package com.dilonexus.sigmalauncher;
 
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.dilonexus.sigmalauncher.apps.AppData;
+import com.dilonexus.sigmalauncher.apps.AppManager;
+import com.dilonexus.sigmalauncher.misc.DataSaver;
+import com.dilonexus.sigmalauncher.misc.Screen;
+import com.dilonexus.sigmalauncher.views.LauncherView;
+import com.dilonexus.sigmalauncher.views.LoadingScreenView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler loadingHandler;
+
+    LauncherView launcher;
+    LoadingScreenView loadingScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Screen.init(this);
+        DataSaver.init(this);
+
+        launcher = new LauncherView(this);
+        setContentView(launcher);
+
+
+        loadApps();
+    }
+
+
+    public void onAppsLoaded(){
+        launcher.removeView(loadingScreen);
+        launcher.build();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void loadApps(){
+        AppManager.init(this);
+
+        loadingScreen = new LoadingScreenView(this);
+        launcher.addView(loadingScreen);
+
+        List<AppData> apps = (List<AppData>) DataSaver.readObject("apps");
+        AppManager.loadAppsResolve();
+        if(apps != null && apps.size() == AppManager.getAppResolveList().size()){
+            AppManager.setApps(apps);
+            onAppsLoaded();
+            return;
+        }
+
+        loadingHandler = new Handler();
+        loadingHandler.post(new Runnable() {
+            public void run() {
+                boolean stop = AppManager.loadNextApp();
+                loadingScreen.setProgress(AppManager.getLoadingProgress());
+                if(!stop) loadingHandler.post(this);
+                else{
+                    onAppsLoaded();
+
+                    DataSaver.saveObject("apps", AppManager.getApps());
+                }
+            }
+        });
     }
 }
