8322fddb62414136d49dbef3fa96bb4940558b5a
Саня Череп <SDesya74@gmail.com>
2019-10-01 23:34:44 +0300

    Connecting new launcher to context

--- app/src/main/java/com/dilonexus/sigmalauncher/MainActivity.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/MainActivity.java
@@ -1,65 +1,34 @@
 package com.dilonexus.sigmalauncher;
 
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.dilonexus.sigmalauncher.apps.AppData;
-import com.dilonexus.sigmalauncher.apps.AppManager;
-import com.dilonexus.sigmalauncher.misc.DataSaver;
-import com.dilonexus.sigmalauncher.views.LauncherView;
-import com.dilonexus.sigmalauncher.views.LoadingScreenView;
+import com.dilonexus.sigmalauncher.misc.FontManager;
 
-import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private Handler loadingHandler;
-
-    LauncherView launcher;
-    LoadingScreenView loadingScreen;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        launcher = new LauncherView(this);
-        setContentView(launcher);
-
-        loadApps();
+        Launcher.show(this);
+        Launcher.startLoadingApps();
     }
 
 
-    public void onAppsLoaded(){
-        launcher.removeView(loadingScreen);
-        launcher.build();
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        int rnd = new Random().nextInt(FontManager.getFonts().size());
+        FontManager.setCurrentFont(FontManager.getFonts().get(rnd));
+        Launcher.invalidate();
+
+        return false;
     }
 
-    @SuppressWarnings("unchecked")
-    public void loadApps(){
-        loadingScreen = new LoadingScreenView(this);
-        launcher.addView(loadingScreen);
-
-        List<AppData> apps = (List<AppData>) DataSaver.readObject("apps");
-        AppManager.loadAppsResolve();
-        if(apps != null && apps.size() == AppManager.getAppResolveList().size()){
-            AppManager.setApps(apps);
-            onAppsLoaded();
-            return;
-        }
-
-        loadingHandler = new Handler();
-        loadingHandler.post(new Runnable() {
-            public void run() {
-                boolean stop = AppManager.loadNextApp();
-                loadingScreen.setProgress(AppManager.getLoadingProgress());
-                if(!stop) loadingHandler.post(this);
-                else{
-                    onAppsLoaded();
-
-                    DataSaver.saveObject("apps", AppManager.getApps());
-                }
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
