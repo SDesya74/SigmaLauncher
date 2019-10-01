0ff1f1cc2c2e236d98bdcf8fbe47704de1163475
Саня Череп <SDesya74@gmail.com>
2019-10-01 21:03:35 +0300

    Access fixed

--- app/src/main/java/com/dilonexus/sigmalauncher/apps/AppData.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/apps/AppData.java
@@ -3,28 +3,28 @@ package com.dilonexus.sigmalauncher.apps;
 import java.io.Serializable;
 
 public class AppData implements Serializable {
-    public long uniqueID;
+    long uniqueID;
     private String packageName;
     public String getPackage(){
         return  packageName;
     }
     private String activityName;
-    public String getActivity() {
+    String getActivity() {
         return activityName;
     }
 
     private String name;
-    public String getName(){
+    String getName(){
         return name;
     }
 
     // region Popularity
     private int popularity;
-    public int getPopularity(){
+    int getPopularity(){
         return popularity;
     }
 
-    public void setPopularity(int popularity){
+    private void setPopularity(int popularity){
         this.popularity = popularity;
     }
 
@@ -37,7 +37,7 @@ public class AppData implements Serializable {
     private long genUniqueId(){
         return Math.round(Math.random() * 10_000_000_000_000_000L);
     }
-    public AppData(String packageName, String activityName, String name, int popularity){
+    AppData(String packageName, String activityName, String name, int popularity){
         this.uniqueID = this.genUniqueId();
         while(AppManager.getAppByID(this.uniqueID) != null) this.uniqueID = genUniqueId();
         this.packageName = packageName;
