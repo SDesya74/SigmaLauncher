1490890cbd51f408639d30b59b23e0b9bd6cecae
Саня Череп <SDesya74@gmail.com>
2019-10-03 21:45:41 +0300

    Fixed text position

--- app/src/main/java/com/dilonexus/sigmalauncher/apps/AppDrawer.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/apps/AppDrawer.java
@@ -1,10 +1,12 @@
 package com.dilonexus.sigmalauncher.apps;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
 import com.dilonexus.sigmalauncher.misc.FontManager;
+import com.dilonexus.sigmalauncher.misc.Options;
 
 public class AppDrawer {
     private static Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -12,15 +14,33 @@ public class AppDrawer {
 
 
     public static void drawApp(Canvas canvas, int x, int y, AppStyle style){
+        if(Options.APP_BOUNDS_SHOWING){
+            paint.setColor(Color.RED);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(4);
+            int sx = x - 1;
+            int sy = y - 1;
+
+            int ex = x + 2 + style.bounds.width();
+            int ey = y + 2 + style.bounds.height();
+            canvas.drawRect(sx, sy, ex, ey, paint);
+        }
+
+
         paint.setColor(style.backColor);
 
         textPaint.setTextSize(style.getPaintTextSize());
         textPaint.setColor(style.textColor);
         textPaint.setTypeface(FontManager.getCurrentFont());
 
-        canvas.drawRect(x, y, x + style.bounds.width(), y + style.bounds.height(), paint);
-        canvas.drawText(
-                style.getLabel(),
+        canvas.drawRect(
+                x,
+                y,
+                x + style.bounds.width(),
+                y + style.bounds.height(),
+                paint);
+
+        canvas.drawText(style.getLabel(),
                 x + (style.bounds.width() - style.textBounds.width()) / 2,
                 y + (style.bounds.height() + style.textBounds.height()) / 2, textPaint);
     }
