d4b4ba7e00f34328dbccca646208014bac25668d
Саня Череп <SDesya74@gmail.com>
2019-10-03 21:44:31 +0300

    Add options

---
+++ app/src/main/java/com/dilonexus/sigmalauncher/misc/Options.java
@@ -0,0 +1,7 @@
+package com.dilonexus.sigmalauncher.misc;
+
+public class Options {
+    public static boolean APP_CACHE_ENABLED = false;
+    public static boolean APP_BOUNDS_SHOWING = true;
+    public static int MIN_TEXT_SIZE = 10;
+}
