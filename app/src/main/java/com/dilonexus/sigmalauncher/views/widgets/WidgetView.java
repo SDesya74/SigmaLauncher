8c7aeb5c820afa26cd874a37bde084ecbe9f2c1b
Саня Череп <SDesya74@gmail.com>
2019-10-04 23:30:21 +0300

    Add parent class to widgets

---
+++ app/src/main/java/com/dilonexus/sigmalauncher/views/widgets/WidgetView.java
@@ -0,0 +1,10 @@
+package com.dilonexus.sigmalauncher.views.widgets;
+
+import android.content.Context;
+import android.view.View;
+
+public class WidgetView extends View {
+    public WidgetView(Context context) {
+        super(context);
+    }
+}
