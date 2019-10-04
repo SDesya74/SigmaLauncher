cd416ce5a513a7bd8dc6a6a4a37b62316f41b5e2
Саня Череп <SDesya74@gmail.com>
2019-10-04 23:09:48 +0300

    Field renamed

--- app/src/main/java/com/dilonexus/sigmalauncher/apps/AppSorter.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/apps/AppSorter.java
@@ -2,10 +2,10 @@ package com.dilonexus.sigmalauncher.apps;
 
 public class AppSorter {
     public enum SortType{
-        APP_NAME, INSTALLATON_TIME, POPULARITY
+        APP_NAME, INSTALL_TIME, POPULARITY
     }
 
-    public static int compareApps(SortType type, AppData first, AppData second){
+    static int compareApps(SortType type, AppData first, AppData second){
         switch (type){
             case APP_NAME:
                 return first.getName().compareTo(second.getName());
@@ -15,7 +15,7 @@ public class AppSorter {
                 int y = second.getPopularity();
                 return (x < y) ? -1 : ((x == y) ? 0 : 1); // Integer.compare requires API 19
 
-            case INSTALLATON_TIME:
+            case INSTALL_TIME:
                 long a = first.installTime;
                 long b = second.installTime;
                 return (a < b) ? -1 : ((a == b) ? 0 : 1);
