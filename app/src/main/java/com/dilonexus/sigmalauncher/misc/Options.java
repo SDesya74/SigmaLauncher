8aa928a700611c8469e01a8029e714b2a9ea6078
Саня Череп <SDesya74@gmail.com>
2019-10-04 11:48:01 +0300

    Some changes

--- app/src/main/java/com/dilonexus/sigmalauncher/misc/Options.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/misc/Options.java
@@ -2,6 +2,6 @@ package com.dilonexus.sigmalauncher.misc;
 
 public class Options {
     public static boolean APP_CACHE_ENABLED = false;
-    public static boolean APP_BOUNDS_SHOWING = true;
+    public static boolean APP_BOUNDS_SHOWING = false;
     public static int MIN_TEXT_SIZE = 10;
 }
