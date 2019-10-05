1d27a16792e5b175e51d15a213e5fd8fd4e39a6f
Саня Череп <SDesya74@gmail.com>
2019-10-06 01:32:07 +0300

    Some changes

--- app/src/main/java/com/dilonexus/sigmalauncher/LauncherApplication.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/LauncherApplication.java
@@ -1,6 +1,5 @@
 package com.dilonexus.sigmalauncher;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 
 import com.dilonexus.sigmalauncher.apps.AppManager;
@@ -9,23 +8,19 @@ import com.dilonexus.sigmalauncher.misc.FontManager;
 import com.dilonexus.sigmalauncher.misc.Screen;
 
 public class LauncherApplication extends android.app.Application {
-
-    @SuppressLint("StaticFieldLeak")
-    private static Context context;
-
     public void onCreate() {
         super.onCreate();
-        context = getApplicationContext();
+        Context context = getApplicationContext();
 
         Screen.init(context);
+
         AppManager.init(context);
+
         DataSaver.init(context);
+
+
         FontManager.init(context);
 
         Launcher.init(context);
-    }
-
-    public static Context getContext() {
-        return LauncherApplication.context;
     }
 }
