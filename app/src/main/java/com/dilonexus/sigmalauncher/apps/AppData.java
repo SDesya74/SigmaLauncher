ad793bbe7f4a813c95b5a1ef0f68f10cbc154def
Саня Череп <SDesya74@gmail.com>
2019-10-06 23:23:44 +0300

    Full rewrite

--- app/src/main/java/com/dilonexus/sigmalauncher/apps/AppData.java
+++ app/src/main/java/com/dilonexus/sigmalauncher/apps/AppData.java
@@ -3,56 +3,18 @@ package com.dilonexus.sigmalauncher.apps;
 import java.io.Serializable;
 
 public class AppData implements Serializable {
-    public long uniqueID;
-    long installTime;
-
-    // region Package
-    private String packageName;
-    public String getPackage(){
-        return  packageName;
-    }
-    // endregion
-
-    // region Activity
-    private String activityName;
-    String getActivity() {
-        return activityName;
-    }
-    // endregion
-
-    // region Name
-    private String name;
-    String getName(){
-        return name;
-    }
-    // endregion
-
-    // region Popularity
-    private int popularity;
-    public int getPopularity(){
-        return popularity;
+    private String key;
+    public String getKey(){
+        return key;
     }
 
-    public void setPopularity(int popularity){
-        this.popularity = popularity;
-    }
+    public int popularity;
+    public boolean hidden;
 
-    public void addPopularity(int popularity){
-        this.setPopularity(getPopularity() + popularity);
-    }
-    // endregion
 
-    // region Unique ID
-    private long genUniqueId(){
-        return Math.round(Math.random() * 10_000_000_000_000_000L);
+    AppData(String key){
+        this.key = key;
+        popularity = 0;
+        hidden = false;
     }
-    AppData(String packageName, String activityName, String name, int popularity){
-        this.uniqueID = this.genUniqueId();
-        while(AppManager.getAppByID(this.uniqueID) != null) this.uniqueID = genUniqueId();
-        this.packageName = packageName;
-        this.activityName = activityName;
-        this.name = name;
-        this.popularity = popularity;
-    }
-    // endregion
 }
